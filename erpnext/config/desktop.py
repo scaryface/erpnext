6f20cb8a73d9cab70e723fcdeed264b0cfa6dbe8
scaryface <adityakanthale@gmail.com>
2021-04-11 17:27:20 +0530

    [did not work] adding new doc type and module

---
+++ erpnext/config/desktop.py
@@ -0,0 +1,14 @@
+[{
+		"module_name": "custom_recruitment",
+		"color": "#8e44ad",
+		"icon": "fa fa-puzzle-piece",
+		"icon": "octicon octicon-rocket",
+		"type": "module"
+},
+{
+		"module_name": _("Test Module"),
+		"color": "#8e44ad",
+		"icon": "fa fa-puzzle-piece",
+		"icon": "octicon octicon-rocket",
+		"type": "module"
+}]
